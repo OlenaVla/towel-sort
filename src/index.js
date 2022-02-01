141871176a0d727092fc157f69606e2d33b34d85
OlenaVla <evlasenko409@gmail.com>
2022-02-01 13:52:31 +0200

    feat: add implementation

--- src/index.js
+++ src/index.js
@@ -7,6 +7,9 @@
 ] */
 module.exports = function towelSort (matrix) {
   let resArray = [];
+  if (arguments.length === 0){
+    return resArray;
+  }
   for (let i = 0; i < matrix.length; i++)
   {
      if (i % 2 === 0)
