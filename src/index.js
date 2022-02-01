888e703dbb8c602586c231384c7e173c6447d930
OlenaVla <evlasenko409@gmail.com>
2022-02-01 13:49:06 +0200

    feat: add func implementation

--- src/index.js
+++ src/index.js
@@ -1,6 +1,21 @@
 
 // You should implement your task here.
-
+/*[
+ [ 1, 2, 3 ],
+ [ 4, 5, 6 ],
+ [ 7, 8, 9 ],
+] */
 module.exports = function towelSort (matrix) {
-  return [];
+  let resArray = [];
+  for (let i = 0; i < matrix.length; i++)
+  {
+     if (i % 2 === 0)
+     {
+       resArray.push(...matrix[i]);
+     }
+     else {
+      resArray.push(...matrix[i].reverse());
+     }
+  }
+  return resArray;
 }
